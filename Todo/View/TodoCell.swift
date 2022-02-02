1488a921853910909a08dac59c0f22ea3b6bd76a
Nikita <nikitademedyuk1@gmail.com>
2022-02-02 17:24:03 +0300

    add file for tableViewCell

---
+++ Todo/View/TodoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoCellTableViewCell.swift
+//  Todo
+//
+//  Created by Никита on 2.02.22.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
